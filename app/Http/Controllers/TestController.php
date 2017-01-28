ecd951e44a449061112f65e7e8171ebd969e9fff
hongyu <240918304@qq.com>
2017-01-28 19:46:53 +0800

    pngtest

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,12 +13,11 @@ class TestController extends Controller
     }
 	
 	public function png(Request $request){
-		header("Content-type: image/png");
-		$text = $request->input('t');
-		$im     = imagecreatefrompng("images/button1.png");
-		$orange = imagecolorallocate($im, 220, 210, 60);
-		$px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
-		imagestring($im, 3, $px, 9, $string, $orange);
+		header ('Content-Type: image/png');
+		$im = @imagecreatetruecolor(120, 20)
+			  or die('Cannot Initialize new GD image stream');
+		$text_color = imagecolorallocate($im, 233, 14, 91);
+		imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
 		imagepng($im);
 		imagedestroy($im);
 	}
