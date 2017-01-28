86ac17f0e359de9e2a6e4242fbd7e751714da17e
hongyu <240918304@qq.com>
2017-01-28 19:29:32 +0800

    testpng

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,4 +11,15 @@ class TestController extends Controller
         $text = $request->input('t');
         echo gd_info();
     }
+	
+	public function png(Request $request){
+		header("Content-type: image/png");
+		$text = $request->input('t');
+		$im     = imagecreatefrompng("images/button1.png");
+		$orange = imagecolorallocate($im, 220, 210, 60);
+		$px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
+		imagestring($im, 3, $px, 9, $string, $orange);
+		imagepng($im);
+		imagedestroy($im);
+	}
 }
